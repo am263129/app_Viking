9745a8f8413acd1aba6b499911f7e6e3662b7ec8
Viking <fashioncstar2019@outlook.com>
2020-03-02 03:52:15 +0300

    step 7-1
    before bottom navigation

---
+++ app/src/main/java/com/official/aptoon/ui/activities/forgotpassActivity.java
@@ -0,0 +1,14 @@
+package com.official.aptoon.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class forgotpassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgotpass);
+    }
+}
